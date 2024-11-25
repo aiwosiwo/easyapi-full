72dc2dce6affe53c36b8e39685a719c6a22cf7ec
aiwosiwo <aiwosiwo@qq.com>
2024-11-25 14:04:28 +0800

    清理无效函数和变量

--- src/main/java/com/easyapi/core/doc/Link.java
+++ src/main/java/com/easyapi/core/doc/Link.java
@@ -1,32 +1,19 @@
 package com.easyapi.core.doc;
 
+import lombok.Data;
+
 /**
  * link
  * <p>
  * licence Apache 2.0, from japidoc
  **/
+@Data
 class Link {
     private String name;
     private String url;
 
     public Link(String name, String url) {
         this.name = name;
-        this.url = url;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
         this.url = url;
     }
 }
